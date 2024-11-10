72abc55445de976cd033f50a45e41ebe071ba102
Kawaii Kultist <rocksmasher87@gmail.com>
2024-11-10 15:01:24 -0600

    Target FPS: 30 -> 60

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 use std::f32::consts::PI;
 
 use player::Player;
-use ruscii::app::{App, State};
+use ruscii::app::{App, Config, State};
 use ruscii::spatial::Vec2;
 use ruscii::terminal::Window;
 use ruscii::drawing::{Pencil, RectCharset};
@@ -24,13 +24,14 @@ const FOV: f32 = PI / 3.0;
 
 
 fn main() {
-    let mut app = App::default();
+    // let mut app = App::default();
+    let mut app = App::config(Config::new().fps(60));
 
     ////////////////////////////////////////////////////////////////
     ////  -----------------  Initialization  -----------------  ////
     ////////////////////////////////////////////////////////////////
 
-    let mut player = Player::new(4.0, 4.0, 0.0, 0.1, 0.1);
+    let mut player = Player::new(4.0, 4.0, 0.0, 0.05, 0.03);
 
     let mut show_stats = false;
 
@@ -38,6 +39,7 @@ fn main() {
     app.run(|app_state: &mut State, window: &mut Window| {
 
         let win_size = window.size();
+        let delta = app_state.dt().as_secs_f32();
 
         ////////////////////////////////////////////////////////////////
         ////  ---------------------  Update  ---------------------  ////
@@ -51,6 +53,7 @@ fn main() {
             }
         }
 
+        // Player Movement
         player.process_keys_down(app_state.keyboard().get_keys_down());
 
 
@@ -67,7 +70,7 @@ fn main() {
         if show_stats {
             let stats_rect_set = RectCharset::double_lines();
             let rect_pos = Vec2::xy(0, 0);
-            let rect_size = Vec2::xy(19, 6); // 19x5
+            let rect_size = Vec2::xy(19, 7); // 19x5
 
             pencil
                 .draw_filled_rect(' ', rect_pos, rect_size)
@@ -75,7 +78,8 @@ fn main() {
                 .draw_text(&format!("Player X: {:.2}", player.get_x()), Vec2::xy(1, 1))
                 .draw_text(&format!("Player y: {:.2}", player.get_y()), Vec2::xy(1, 2))
                 .draw_text(&format!("Player A: {:.2}", player.get_a().to_degrees()), Vec2::xy(1, 3))
-                .draw_text(&format!("Win Size: {:.2}x{:.2}", win_size.x, win_size.y), Vec2::xy(1, 4));
+                .draw_text(&format!("Win Size: {:.2}x{:.2}", win_size.x, win_size.y), Vec2::xy(1, 4))
+                .draw_text(&format!("Delta: {:.4}", delta), Vec2::xy(1, 5));
         }
         
     });
